a53150a6fde97dd24363c74ec95f1e78561a69a4
Sudeep Padalkar <holysatan84@gmail.com>
2018-01-22 15:02:10 +0530

    added patch api for todo API

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
-var {
+const _ = require('lodash');
+const {
   ObjectID
 } = require('mongodb');
-var express = require('express');
-var bodyParser = require('body-parser');
+
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var port = process.env.PORT || 3000;
 
@@ -83,6 +85,36 @@ app.delete('/todos/:id', (req, res) => {
     });
   }).catch((e) => {
     res.status(400).send();
+  });
+
+});
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {
+    $set: body
+  }, {
+    new: true
+  }).then((todo) => {
+    if (!todo) {
+      return res.status(400).send();
+    }
+    return res.send(todo);
+  }).catch((e) => {
+    return res.status(400).send();
   });
 });
 
