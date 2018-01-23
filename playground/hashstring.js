6db53ac0bab6879bb0aad18f095615d29231a170
Sudeep Padalkar <holysatan84@gmail.com>
2018-01-23 18:00:41 +0530

    User Model Schema modified, added jwt tokens when users created

---
+++ playground/hashstring.js
@@ -0,0 +1,16 @@
+const {
+  SHA256
+} = require('crypto-js');
+
+const jwt = require('jsonwebtoken');
+
+var data = {
+  id: 10
+}
+
+var token = jwt.sign(data, '123abc');
+
+var decoded = jwt.verify(token, '123abc');
+
+console.log(token);
+console.log(decoded);
